ed5d69ce601fce7feb735c196a63aff1eff13a48
キリリ <kiriri_ri@126.com>
2023-03-17 18:04:58 +0800

    feat: 改变响应格式

--- lib/utils/api_util.dart
+++ lib/utils/api_util.dart
@@ -3,31 +3,32 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 
 class Resp {
-  String code;
+  int code;
   Map<String, dynamic> data;
-  String msg;
+  String errMsg;
 
-  Resp(this.code, this.data, this.msg);
+  Resp(this.code, this.data, this.errMsg);
 
   Resp.formJson(Map<String, dynamic> json)
       : code = json['code'],
         data = json['data'],
-        msg = json['msg'];
-}
+        errMsg = json['errMsg'];
+} 
 
 class ApiClient {
   static Future<Resp> get(String path, {Map<String, String>? pathVariable,
     Map<String, dynamic>? query}) async {
-    String url = "http://127.0.0.1:4523/mock/2137957$path";
+    String url = "http://101.43.168.250:10011$path";
     if (pathVariable != null) {
       pathVariable.forEach((key, value) {
         url = url.replaceAll(":$key", value);
       });
     }
-
-    var resp = await Dio().get(url, queryParameters: query);
+    var dio = Dio();
+    dio.options.headers = {'Authorization':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJraXJpcml4IiwiZXhwIjoxNjgwNjcwMzA4LCJpc3MiOiJhZG1pbiJ9.WcNfTlYnwgSKvE4cC0CBuw-JAMFuh27ABt331eCInl4'};
+    var resp = await dio.get(url, queryParameters: query);
     if (resp.statusCode != 200) {
-      return Resp("50${resp.statusCode}", {}, "http请求错误");
+      return Resp(resp.statusCode!, {}, "http请求错误");
     }
     return Resp.formJson(resp.data);
   }
